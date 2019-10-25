73d2ad715832857b3f9decce966a9ed97da4563f
dxvgef <dxvgef@outlook.com>
2019-10-25 15:03:13 +0800

    简化读取配置文件的逻辑

--- src/global/config.go
+++ src/global/config.go
@@ -1,10 +1,7 @@
 package global
 
 import (
-	"errors"
 	"flag"
-	"path/filepath"
-	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -61,37 +58,12 @@ var Config struct {
 	}
 }
 
-// 解析配置文件路径
-func parseFilePath(configFilePath string) (configName string, configType string) {
-	if configFilePath == "" {
-		return
-	}
-	fullExt := filepath.Ext(configFilePath)
-	if fullExt == "" {
-		return
-	}
-	extArr := strings.Split(fullExt, ".")
-	if len(extArr) > 1 {
-		configType = extArr[1]
-	}
-	extPos := strings.LastIndex(configFilePath, fullExt)
-	configName = configFilePath[:extPos]
-	return
-}
-
 // 设置全局配置变量
 func SetConfig() error {
 	configFilePath := flag.String("c", "./config.toml", "配置文件路径")
 	flag.Parse()
 
-	configName, configType := parseFilePath(*configFilePath)
-	if configName == "" || configType == "" {
-		return errors.New("无法读取配置文件")
-	}
-	viper.SetConfigName(configName) // 配置文件名，不需要扩展名
-	viper.SetConfigType(configType) // 文件类型
-	viper.AddConfigPath(".")        // 文件路径
-
+	viper.SetConfigFile(*configFilePath) // 配置文件路径
 	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
