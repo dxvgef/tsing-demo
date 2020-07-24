576b3d30b222671829b1be692c2d4abafa6384bd
dxvgef <dxvgef@outlook.com>
2020-07-24 11:44:36 +0800

    优化snowflake id

--- src/global/id.go
+++ src/global/id.go
@@ -1,15 +1,20 @@
 package global
 
-import "github.com/bwmarrin/snowflake"
+import (
+	"math/rand"
+	"time"
+
+	"github.com/bwmarrin/snowflake"
+)
 
 // IDnode ID节点实例
 var IDnode *snowflake.Node
 
 // SetIDnode 设置ID节点
 func SetIDnode() (err error) {
-	if LocalConfig.Snowflake.Epoch > 0 {
-		snowflake.Epoch = LocalConfig.Snowflake.Epoch
-	}
-	IDnode, err = snowflake.NewNode(LocalConfig.Snowflake.Node)
+	snowflake.Epoch = time.Now().Unix()
+	rand.Seed(rand.Int63n(time.Now().UnixNano()))
+	node := 0 + rand.Int63n(1023)
+	IDnode, err = snowflake.NewNode(node)
 	return
 }
