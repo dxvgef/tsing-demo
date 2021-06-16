6ff5190897471a66b8ebc8835860f98ac35a65c6
dxvgef <dxvgef@outlook.com>
2021-06-16 18:10:04 +0800

    update

--- src/service/router.go
+++ src/service/router.go
@@ -13,7 +13,7 @@ func setRouter() {
 	var ts handler.TestSession
 	testSession.PUT("/", ts.Put)
 	testSession.GET("/:key", ts.Get)
-	testSession.GET("/:key", ts.Get)
+	testSession.DELETE("/", ts.Destroy)
 
 	// 定义路由组，同时注册一个处理器做为中间件
 	// secretRouter := engine.Group("/secret", handler.Auth)
